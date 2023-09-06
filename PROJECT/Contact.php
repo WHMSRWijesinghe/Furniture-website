33f7777594a27bf65dcabaa179dcc6231992c40f
WHMSRWijesinghe <144237581+WHMSRWijesinghe@users.noreply.github.com>
2023-09-06 17:45:46 +0530

    Add files via upload

---
+++ PROJECT/Contact.php
@@ -0,0 +1,24 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $quote = $_POST['quote'];
+    $massage = $_POST['massage'];
+
+    $to = 'shahanrawidu122@gmail.com';
+    $subject = 'From Submission';
+    $massage = "Name: " .$name. "\n". "Email: " .$email. "\n".  "Phone: " .$phone. "\n". "Quote: " .$quote. "\n\n". "Massage: " . $massage;
+          
+
+    
+
+
+
+
+    
+
+
+
+
+
+?>
